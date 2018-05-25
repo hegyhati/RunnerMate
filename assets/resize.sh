1bb7eb9715f8c84462efe02d77a5420f49000120
Mate Hegyhati <hegyhati.work@gmail.com>
2018-05-25 18:39:44 +0200

    Image rescaling

---
+++ assets/resize.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir "$1/backup"
+
+
+for i in `ls $1/ | grep -E "jpg|JPG" `
+do
+  cp "$1/$i" "$1/backup/"
+  convert -resize 1240x1240\> "$1/$i" "$1/$i"
+done
+
+convert -resize 640x640\> "$1/cover.jpg" "$1/cover.jpg"
