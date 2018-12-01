9388b342567ada9d585cae8f9954f728e5742da0
Mate Hegyhati <hegyhati.work@gmail.com>
2018-12-02 00:53:46 +0100

    OldGarminGoodGarmin

--- assets/resize.sh
+++ assets/resize.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mkdir "$1/backup"
+mkdir -p "$1/backup"
 
 
-for i in `ls $1/ | grep -E "jpg|JPG" `
+for i in `ls $1/ | grep -E "jpg|JPG|png|PNG" `
 do
   cp "$1/$i" "$1/backup/"
   convert -resize 1240x1240\> "$1/$i" "$1/$i"
